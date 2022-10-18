e942d0477d6a19e936566512f7d341dfa6616096
Kevin Chu <14960080+imkevinchu@users.noreply.github.com>
2022-10-17 20:31:46 -0700

    init commit

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    assetModuleFilename: "assets/[hash][ext][query]",
+  },
+  devtool: "source-map",
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "The Weather",
+      filename: "index.html",
+      template: "src/template.html",
+      favicon: "src/img/favicon.ico",
+    }),
+  ],
+};
